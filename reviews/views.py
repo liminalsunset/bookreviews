80c81ed12ee73253fb305654de34c1baacf366aa
dev-yoga <spin_shoppe@icloud.com>
2022-10-13 11:21:50 -0700

    Hello, name

--- reviews/views.py
+++ reviews/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    name = request.GET.get("name") or "world"
+    return HttpResponse("Hello, {}!". format(name))
